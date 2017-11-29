57e5b60de6cd26858be06c66c26a2222cd20e352
Tyler Maule <mauletj@whitman.edu>
2017-11-28 21:12:28 -0800

    w/ exception catching

--- simulationHabitat2.py
+++ simulationHabitat2.py
@@ -1,4 +1,6 @@
 import random
+import pygame
+
 #Just some ideas on implementation of simulation
 #very object oriented. we could go other directions
 w,h = 20, 20
@@ -6,21 +8,29 @@ board = [[[] for x in range(w)] for y in range(h)] #array of array of arrays
 #print(board)
 
 class Plant:
+
 	def __init__(self,x,y):
 		self.x=x
 		self.y=y
+
 	def update(self):
 		return
 
 class Prey:
+
 	def __init__(self,x,y,repro):
 		self.count = 50
 		self.x=x
 		self.y=y
 		self.repro=repro
+
 	def update(self):
 		rand=random.randint(1,4) #needs collisions checks.
-		board[self.x][self.y].remove(self) #think about modulo looping around maybe
+		try:
+			board[self.x][self.y].remove(self) #think about modulo looping around maybe
+		except ValueError:
+			return
+
 		if(rand==1 and self.x+1<20):
 			self.x=self.x+1
 		if(rand==2 and self.x-1>-1):
@@ -29,41 +39,70 @@ class Prey:
 			self.y=self.y+1
 		if(rand==4 and self.y-1>-1):
 			self.y=self.y-1
+
 		if (len(board[self.x][self.y]) > 0):
-			for u in board[self.x][self.y]:
-				if type(u) == 'Predator':
-                                        characters.remove(self)
-					pass
-				if type(u) == 'Plant':
+			for u in (board[self.x][self.y]):
+				if type(u) == Predator:
+					print('prey eaten')
+					characters.remove(self)
+					return
+				if type(u) == Plant:
+					print('prey eat plant')
 					board[self.x][self.y].remove(u)
-                                        characters.remove(u)
+					characters.remove(u)
 					self.count = 50
 					board[self.x][self.y].append(self)
-                else:	
+					return
+				else:
+					board[self.x][self.y].append(self)
+					self.count -= 1
+					return
+		else:
 			board[self.x][self.y].append(self)
-		
+
 class Predator:
+
 	def __init__(self,x,y,repro):
-                self.x=x
-                self.y=y
-                self.repro=repro
+				self.count = 50
+				self.x=x
+				self.y=y
+				self.repro=repro
+
 	def update(self):
-                rand=random.randint(1,4) #needs collisions checks.
-                board[self.x][self.y].remove(self) #think about modulo looping around maybe
-                if(rand==1 and self.x+1<20):
-                        self.x=self.x+1
-                if(rand==2 and self.x-1>-1):
-                        self.x=self.x-1
-                if(rand==3 and self.y+1<20):
-                        self.y=self.y+1
-                if(rand==4 and self.y-1>-1):
-                        self.y=self.y-1 
-                board[self.x][self.y].append(self)
+				rand=random.randint(1,4) #needs collisions checks.
+				try:
+					board[self.x][self.y].remove(self) #think about modulo looping around maybe
+				except ValueError:
+					return
+
+				if(rand==1 and self.x+1<20):
+						self.x=self.x+1
+				if(rand==2 and self.x-1>-1):
+						self.x=self.x-1
+				if(rand==3 and self.y+1<20):
+						self.y=self.y+1
+				if(rand==4 and self.y-1>-1):
+						self.y=self.y-1
+
+				if (len(board[self.x][self.y]) > 0):
+					for u in (board[self.x][self.y]):
+						if type(u) == Prey:
+							board[self.x][self.y].remove(u)
+							characters.remove(u)
+							self.count = 50
+							board[self.x][self.y].append(self)
+							return
+						else:
+							board[self.x][self.y].append(self)
+							return
+				else:
+					board[self.x][self.y].append(self)
 
 characters=[]
-time_tot=100
-dt=0.1
+time_tot=1000
+dt=0.01
 current_t=0
+
 #randomly initalize 10 plants, 5 prey, 2 predators
 for i in range(10): #plant=1
 	x=random.randint(0, 19)
@@ -73,24 +112,48 @@ for i in range(10): #plant=1
 	board[x][y].append(plant)
 
 for i in range(5): #prey =2
-        x=random.randint(0, 19)
-        y=random.randint(0, 19)
-        prey=Prey(x,y,3)
-        characters.append(prey)	
-        board[x][y].append(prey)
+		x=random.randint(0, 19)
+		y=random.randint(0, 19)
+		prey=Prey(x,y,3)
+		characters.append(prey)
+		board[x][y].append(prey)
 
 for i in range(2):#predator =3
-        x=random.randint(0, 19)
-        y=random.randint(0, 19)
-        predator=Predator(x,y,1)
-        characters.append(predator)
-        board[x][y].append(predator)
+		x=random.randint(0, 19)
+		y=random.randint(0, 19)
+		predator=Predator(x,y,1)
+		characters.append(predator)
+		board[x][y].append(predator)
 
 
-while(current_t<time_tot):
-	current_t=current_t+dt
-	for i in characters:
-		i.update()
-	for j in range(20):
-		print(board[j])
-	print("\n")
+pygame.init()
+screen = pygame.display.set_mode((800,800))
+done = False
+
+for i in range(0,800,40):
+	for j in range(0,800,40):
+		r_color = 0 + random.randint(0,60)
+		g_color = 60 + random.randint(0,50)
+		b_color = 0 + random.randint(0,60)
+		pygame.draw.rect(screen, (r_color, g_color, b_color),
+				pygame.Rect(i,j,40,40))
+
+while not done:
+	for event in pygame.event.get():
+
+		if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT and ((current_t<time_tot)):
+			current_t+=+dt
+			for i in characters:
+				i.update()
+				pygame.draw.rect(screen,(random.randint(40,100),0,0),pygame.Rect(i.x * 40,i.y * 40,40,40))
+				pygame.display.update()
+		if event.type == pygame.QUIT:
+			done = True
+		pygame.display.update()
+		pygame.display.flip()
+
+endnum = 0
+for j in range(20):
+	for i in range(20):
+		endnum += len(board[j][i])
+print (endnum)
