09b9aab2bb06839b57f32f5e0f4062fcfa05ae65
Tyler Maule <mauletj@whitman.edu>
2017-11-30 17:29:57 -0800

    Add files via upload

--- HungryHabitat3.py
+++ HungryHabitat3.py
@@ -21,12 +21,24 @@ class Plant(object):
 		self.y=y
 
 	def update(self):
+		if (random.randint(0,1000) > 975):
+			if (random.random<=.5):
+				plant_x = (self.x+1)%20
+			else:
+				plant_x = (self.x-1)%20
+			if (random.random<=.5):
+				plant_y = (self.y+1)%20
+			else:
+				plant_y = (self.y-1)%20
+			plant = Plant(plant_x,plant_y)
+			board[plant_x][plant_y].append(plant)
+			characters.append(plant)
 		return
 
 class Prey(object):
 
 	def __init__(self,x,y,repro):
-		self.count = 50
+		self.count = 200
 		self.x=x
 		self.y=y
 		self.repro=repro
@@ -37,6 +49,11 @@ class Prey(object):
 			board[self.x][self.y].remove(self) #think about modulo looping around maybe
 		except ValueError:
 			return
+
+		if (self.count <= 0):
+			characters.remove(self)
+			return
+
 
 		if(rand==1 and self.x+1<20):
 			self.x=self.x+1
@@ -57,20 +74,32 @@ class Prey(object):
 					print('prey eat plant')
 					board[self.x][self.y].remove(u)
 					characters.remove(u)
-					self.count = 50
+					self.count = 300
 					board[self.x][self.y].append(self)
+					#plant=Plant(self.x, self.y)
+					#characters.append(plant)
+					plant_countdown = 25
+					plant_locations.append((self.x,self.y))
 					return
-				else:
+				elif type(u) == Prey:
 					board[self.x][self.y].append(self)
 					self.count -= 1
+					if (random.random() > self.repro) and (self.count > 200):
+						child_x = (self.x + random.randint(-3,3)) % 20
+						child_y = (self.y + random.randint(-3,3)) % 20
+						child_prey = Prey(child_x,child_y,0.7)
+						board[child_x][child_y].append(child_prey)
+						characters.append(child_prey)
 					return
 		else:
 			board[self.x][self.y].append(self)
+			self.count -= 1
+		
 
 class Predator(object):
 
 	def __init__(self,x,y,repro):
-				self.count = 50
+				self.count = 200
 				self.x=x
 				self.y=y
 				self.repro=repro
@@ -81,6 +110,12 @@ class Predator(object):
 					board[self.x][self.y].remove(self) #think about modulo looping around maybe
 				except ValueError:
 					return
+
+				
+				if (self.count <= 0):
+					characters.remove(self)
+					return
+
 
 				if(rand==1 and self.x+1<20):
 						self.x=self.x+1
@@ -96,16 +131,29 @@ class Predator(object):
 						if type(u) == Prey:
 							board[self.x][self.y].remove(u)
 							characters.remove(u)
-							self.count = 50
+							self.count = 300
 							board[self.x][self.y].append(self)
 							return
-						else:
+						elif type(u) == Predator:
+							print('endless')
 							board[self.x][self.y].append(self)
 							self.count -= 1
+							if (random.random() > self.repro) and (self.count > 200):
+								child_x = (self.x + random.randint(-3,3)) % 20
+								child_y = (self.y + random.randint(-3,3)) % 20
+								if(self.y != child_y) and (self.x != child_x):
+									child_pred = Predator(child_x,child_y,self.repro)
+									board[child_x][child_y].append(child_pred)
+									characters.append(child_pred)
 							return
+					else:
+						board[self.x][self.y].append(self)
+						self.count -= 1
 				else:
 					board[self.x][self.y].append(self)
 					self.count -= 1
+					return
+
 
 characters=[]
 time_tot=1000
@@ -113,24 +161,26 @@ dt=0.01
 current_t=0
 
 #randomly initalize 10 plants, 5 prey, 2 predators
-for i in range(10): #plant=1
+plant_locations = []
+for i in range(20): #plant=1
 	x=random.randint(0, 19)
 	y=random.randint(0, 19)
+	plant_locations.append((x,y))
 	plant=Plant(x,y)
 	characters.append(plant)
 	board[x][y].append(plant)
 
-for i in range(5): #prey =2
+for i in range(15): #prey =2
 		x=random.randint(0, 19)
 		y=random.randint(0, 19)
-		prey=Prey(x,y,3)
+		prey=Prey(x,y,0.7)
 		characters.append(prey)
 		board[x][y].append(prey)
 
-for i in range(2):#predator =3
+for i in range(3):#predator =3
 		x=random.randint(0, 19)
 		y=random.randint(0, 19)
-		predator=Predator(x,y,1)
+		predator=Predator(x,y,.5)
 		characters.append(predator)
 		board[x][y].append(predator)
 
@@ -138,6 +188,8 @@ for i in range(2):#predator =3
 pygame.init()
 screen = pygame.display.set_mode((800,800))
 done = False
+
+chicken = pygame.image.load('chicken2.png')
 
 def draw_background():
 	for i in range(0,800,40):
@@ -147,25 +199,38 @@ def draw_background():
 			b_color = 0 + random.randint(0,60)
 			pygame.draw.rect(screen, (0,60,0),
 					pygame.Rect(i,j,40,40))
+plant_countdown = 10
 
 while not done:
 	for event in pygame.event.get():
+		plant_countdown = max(plant_countdown-1,0)
+		if plant_countdown == 0:
+			for l in plant_locations:
+				if(len(board[l[0]][l[1]])==0) and (random.randint(0,1000)>990):
+					print('plant regrowing')
+					plant = Plant(l[0],l[1])
+					board[plant.x][plant.y].append(plant)
+					characters.append(plant)
+				else:
+					plant_countdown = 10
+			plant_locations = []	
+			plant_countdown = 10
 		for p in range(1):
 			draw_background()
 			current_t+=+dt
 			for i in characters:
-                            print(i)
-                            print(type(i))
                             i.update()
                             color = (0,0,0)
                             if type(i) == Prey:
-                                color = (0,0,100,50)
+                                screen.blit(chicken, (i.x * 40,i.y * 40))
                             elif type(i) == Predator:
                                 print('count',i.count)
-                                color = (max(i.count*5,0),0,0,50)
+                                color = (60,0,0,50)
+				pygame.draw.rect(screen,(color),pygame.Rect(i.x * 40,i.y * 40,40,40))
+
                             elif type(i) == Plant:
                                 color = (100,0,100,50)
-                            pygame.draw.rect(screen,(color),pygame.Rect(i.x * 40,i.y * 40,40,40))
+                            	pygame.draw.rect(screen,(color),pygame.Rect(i.x * 40,i.y * 40,40,40))
                             pygame.display.update()
 
 		if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT and ((current_t<time_tot)):
@@ -177,6 +242,5 @@ while not done:
 
 endnum = 0
 for j in range(20):
-	for i in range(20):
-		endnum += len(board[j][i])
+	print(board[j])
 print (endnum)
